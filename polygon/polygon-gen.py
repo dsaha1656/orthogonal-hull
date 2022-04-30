3aa14bc6ea6f5ad5336996d82407bf57b0babe3b
Dibyendu Saha <dsaha1656@gmail.com>
2022-04-30 16:43:16 +0530

    orthogonal convex hull beta

--- polygon/polygon-gen.py
+++ polygon/polygon-gen.py
@@ -2,39 +2,27 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# input_image = "sample.jpg"
-# input_image = "sample2.jpg"
 input_image = "sample3.jpg"
-# input_image = "sample4.jpg"
 
-src = cv2.imread(input_image, 1) # read input image
-# find max and min of the image
+src = cv2.imread(input_image, 1) 
 max_threshold = np.max(src)
-# second max value
 min_threshold = np.max(src[src < max_threshold])
-gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY) # convert to grayscale
-blur = cv2.blur(gray, (3, 3)) # blur the image
-ret, thresh = cv2.threshold(blur, min_threshold, max_threshold, cv2.THRESH_BINARY)
+src = cv2.imread(input_image, 1) 
+gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY) 
+ret, thresh = cv2.threshold(gray, min_threshold, max_threshold, cv2.THRESH_BINARY)
+plt.imshow(gray)
 
-plt.imshow(thresh)
-
-# Finding contours for the thresholded image
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-# create hull array for convex hull points
+
 hull = []
-# calculate points for each contour
 for i in range(len(contours)):
-    # creating convex hull object for each contour
     hull.append(cv2.convexHull(contours[i], False))
-# create an empty black image
+
 drawing = np.zeros((thresh.shape[0], thresh.shape[1], 3), np.uint8)
-color_contours = (0, 255, 0) # green - color for contours
-color = (255, 0, 0) # blue - color for convex hull BGR
-
-
+color_contours = (0, 255, 0) 
+color = (255, 0, 0) 
 print("Counture found : ", len(contours))
 
-# find the largest contour
 outer_contour_index = 0
 outer_contour_area = 0
 for i in range(len(contours)):
@@ -44,18 +32,32 @@ for i in range(len(contours)):
 print("Largest contour index : ", outer_contour_index)
 
 outer_contour = contours[outer_contour_index]
+cv2.drawContours(drawing, contours, outer_contour_index, color_contours, 1, 8, hierarchy)
+cv2.drawContours(drawing, hull, outer_contour_index, color, 1, 8)
 
-print(outer_contour)
-
-for i in range(len(outer_contour)):
-    # make drawing[outer_contour[i][1]][outer_contour[i][0]] = color with a width of 3
-    cv2.circle(drawing, (outer_contour[i][0][0], outer_contour[i][0][1]), 2, color, -1)
-
+hull_points = np.asarray(hull[outer_contour_index])
 fig = plt.figure(figsize=(10, 10))
 plt.imshow(drawing)
 
+drawing = np.zeros((thresh.shape[0], thresh.shape[1], 3), np.uint8)
+color = (255, 0, 0) 
 
+fig = plt.figure(figsize=(10, 10))
+for i in range(len(hull_points)):
+    first_point = hull_points[i][0]
+    last_point = hull_points[(i+1)%len(hull_points)][0]
+    perp_point = (first_point[0], last_point[1])
+    plt.plot([first_point[0], perp_point[0]], [first_point[1], perp_point[1]], color)
+    plt.plot([perp_point[0], last_point[0]], [perp_point[1], last_point[1]], color)
 
+plt.imshow(drawing)
 
+drawing = np.zeros((thresh.shape[0], thresh.shape[1], 3), np.uint8)
+color_contours = (0, 255, 0) 
+color = (255, 0, 0) 
 
+for c_index in range(len(contours[outer_contour_index])):
+    drawing[contours[outer_contour_index][c_index][0][1], contours[outer_contour_index][c_index][0][0]] = color
 
+fig = plt.figure(figsize=(10, 10))
+plt.imshow(drawing)
